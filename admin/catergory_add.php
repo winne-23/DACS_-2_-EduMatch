41192a1759e26dce488e4b319cd14804b7b26c37
tran manh thang <thangtm.23it@vku.udn.vn>
2025-07-26 18:05:31 +0700

    chiều ngày 26

---
+++ admin/catergory_add.php
@@ -0,0 +1,16 @@
+<?php
+include "header.php";
+include "slide.php"
+?>
+<div class="adimin_content_right">
+    <h1>Thêm Menu</h1>
+    <div class="admin_content_right_category_add">
+        <form action="" method="POST">
+            <input type="text" placeholder="Nhập tên Menu">
+            <button type="submit">Thêm</button>
+        </form>
+    </div>
+</div>
+</body>
+
+</html>
---
+++ admin/slider.php
@@ -0,0 +1,22 @@
+ <section class="admin_conten">
+        <div class="admin_content_left">
+            <ul>
+                <li><a href="">Menu</a>
+                    <ul>
+                        <li><a href="">Thêm menu</a></li>
+                        <li><a href="">Danh sách menu</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Sub_menu</a>
+                    <ul>
+                        <li><a href="">Thêm Sub_menu</a></li>
+                        <li><a href="">Danh sách Sub_menu</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Content</a>
+                <ul>
+                        <li><a href="">Thêm Content</a></li>
+                        <li><a href="">Danh sách Content</a></li>
+                    </ul></li>
+            </ul>
+        </div>
